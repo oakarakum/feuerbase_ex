105c661cfe35cc9fe4e42717775a8c77bdff63dc
oakarakum <onur.ardakarakum@outlook.com>
2022-12-21 12:50:34 +0300

    visiblity çalışıyor

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -4,8 +4,10 @@ import 'package:get/get.dart';
 import '../routes/app_pages.dart';
 
 class LoginController extends GetxController {
+  var isPasswordHidden = true.obs;
   void pageChanger() {
     // Get.to(() => RegisterScreen());
     Get.toNamed(Routes.REGISTER);
   }
+  void 
 }
