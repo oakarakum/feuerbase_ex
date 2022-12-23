27a5ee0180dba87ceed1847dac32119b79762012
oakarakum <onur.ardakarakum@outlook.com>
2022-12-23 13:47:11 +0300

    search bar tamamlandı

--- lib/modules/homepage/homepage_screen.dart
+++ lib/modules/homepage/homepage_screen.dart
@@ -1,6 +1,7 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class HomepageScreen extends StatelessWidget {
   const HomepageScreen({super.key});
@@ -8,8 +9,81 @@ class HomepageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(child: Text("HomePage")),
-    );
+        backgroundColor: Color(0xff000000),
+        appBar: AppBar(
+          toolbarHeight: 1.h,
+          backgroundColor: Color(0xff000000),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5.w),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: .5.h),
+                  child: Text(
+                    "Blogs",
+                    style: TextStyle(
+                        color: Color(0xffFFFFFF),
+                        fontWeight: FontWeight.w700,
+                        fontSize: 3.5.h),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 1.h),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 72.w,
+                        child: TextFormField(
+                          style: TextStyle(color: Color(0xffFFFFFF)),
+                          keyboardType: TextInputType.emailAddress,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          textAlignVertical: TextAlignVertical.center,
+                          decoration: InputDecoration(
+                            isDense: true,
+                            suffixIcon: Icon(
+                              Icons.search,
+                              color: Color(0xffFFFFFF),
+                              size: 3.h,
+                            ),
+                            hintText: "Search",
+                            hintStyle: TextStyle(
+                                color: Color(0xffFFFFFF),
+                                letterSpacing: 2.5,
+                                fontWeight: FontWeight.w500),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color(0xffFFFFFF), width: 0.3.w)),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color(0xffFFFFFF), width: 0.4.w)),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 4.w),
+                      Container(
+                        height: 6.3.h,
+                        width: 14.w,
+                        decoration: BoxDecoration(
+                          color: Color(0xff29B6F6),
+                          borderRadius: BorderRadius.circular(2.w),
+                        ),
+                        child: Center(
+                          child: Image.asset(
+                            "assets/icon.png",
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
